7192a27882395a49159753f000fbd0087461a9c4
asofimp <asofimp@gmail.com>
2026-02-25 23:19:12 -0600

    feat: response gin

--- pkg/common/common.go
+++ pkg/common/common.go
@@ -1,10 +1,5 @@
 package common
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
 // Estructura base para todas las respuestas
 type APIResponse struct {
 	Code    string      `json:"code"`
@@ -20,39 +15,21 @@ type ErrorInfo struct {
 }
 
 // Respuesta exitosa
-func SuccessResponse(w http.ResponseWriter, message string, data interface{}, code string) {
-	response := APIResponse{
+func SuccessResponse(message string, data interface{}, code string) APIResponse {
+	return APIResponse{
 		Code:    code,
 		Message: message,
 		Data:    data,
 	}
-
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(response)
-}
-
-// Respuesta exitosa para creación
-func CreatedResponse(w http.ResponseWriter, message string, data interface{}, code string) {
-	response := APIResponse{
-		Code:    code,
-		Message: message,
-		Data:    data,
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(response)
 }
 
 // Respuesta de error
-func ErrorResponse(w http.ResponseWriter, statusCode int, code string, message string, data *string) {
-	response := ErrorInfo{
+func ErrorResponse(statusCode int, code string, message string, data *string) ErrorInfo {
+	return ErrorInfo{
 		Code:    code,
 		Message: message,
 		Data:    data,
 	}
-
-	w.WriteHeader(statusCode)
-	json.NewEncoder(w).Encode(response)
 }
 
 func (e *ErrorInfo) Error() string {
